580bc4f9ba648a9e553088da357df4c51abc6476
Yves Talboom <yves.talboom@gmail.com>
2018-01-01 07:35:16 +0100

    add healthz endpoint

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var app = express();
 app.get('/', function(req, res) {
   res.send('Hello World');
 });
+app.get('/healthz', function(req, res) {
+  // do app logic here to determine if app is truly healthy
+  // you should return 200 if healthy, and anything else will fail
+  // if you want, you should be able to restrict this to localhost (include ipv4 and ipv6)
+  res.send('I am happy and healthy\n');
+});
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
